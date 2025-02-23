bf0828bce30d38415b1c15e9d59b0854b62126f0
clione12 <cci1024@163.com>
2025-02-23 23:19:19 +0800

    feat(notebook): Add cpp02 notebook

--- week02/examples/overflow.cpp
+++ week02/examples/overflow.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
     int a = 56789;
     int b = 56789;
+    cout << std::hex << a << endl;
     int c = a * b;
+    cout << std::hex << c << endl;
+
     cout << "c = " << c << endl;
 
-    // unsigned int c1 = a * b; //danger operation
-    // cout << "c1 = " << c1 << endl;
+    unsigned int c1 = a * b; //danger operation
+    cout << "c1 = " << c1 << endl;
     return 0;
 }
